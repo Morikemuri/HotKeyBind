6197fdfdfe8f8236fd6b20cc26a41764fe15a868
Morikemuri <42452214+Morikemuri@users.noreply.github.com>
2026-03-04 16:59:09 +0200

    add src/main/java/net/kyrptonaught/hotkeybind/GuiKeyHandler.java

---
+++ src/main/java/net/kyrptonaught/hotkeybind/GuiKeyHandler.java
@@ -0,0 +1,18 @@
+package net.kyrptonaught.hotkeybind;
+
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.event.InputEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.kyrptonaught.hotkeybind.gui.MacrosScreen;
+
+public class GuiKeyHandler {
+
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+        Minecraft mc = Minecraft.getInstance();
+        if (mc.player == null) return;
+        if (HotKeybindMod.OPEN_GUI_KEY.consumeClick()) {
+            mc.setScreen(new MacrosScreen(null));
+        }
+    }
+}
